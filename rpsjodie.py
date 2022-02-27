a7ef189337b6bf694c27704a4a45bcde1569cb8e
Adrian Samuel <adrian.samuel@machinemax.com>
2022-02-27 22:16:31 +0000

    [Rock Paper Scissors Game] Remove redundant code and fix options
    - Removing redundant else and elseif statements
    - Changing random function to only go up to 2

--- rpsjodie.py
+++ rpsjodie.py
@@ -1,33 +1,35 @@
 import random
 
-rock = 0
-paper = 1
-scissors = 2
-
-
 def player_option():
     user_choice = input("Choose Rock(r), paper(p) or scissors(s): ").lower()
+   
     if user_choice in ["r", "rock"]:
         user_choice = "rock"
-    elif user_choice in ["p", "paper"]:
+   
+    if user_choice in ["p", "paper"]:
         user_choice = "paper"
-    elif user_choice in ["s", "scissors"]:
+    
+    if user_choice in ["s", "scissors"]:
         user_choice = "scissors"
-    else:
+    
+    if user_choice not in ["r", "rock", "p", "paper", "s", "scissors"]:
         print("Please enter r, p, or s.")
-        player_option()
+        user_choice = player_option()
+
     return user_choice
-#need to correct return when player does not enter correct value
 
 
 def computer_option():
-    computer_choice = random.randint(0, 3)
+    computer_choice = random.randint(0, 2)
+   
     if computer_choice == 0:
         computer_choice = "rock"
-    elif computer_choice == 1:
+    
+    if computer_choice == 1:
         computer_choice = "paper"
-    else:
+    if computer_choice == 2:
         computer_choice = "scissors"
+
     return computer_choice
 
 
@@ -39,26 +41,26 @@ computer_choice = computer_option()
 if user_choice == "rock":
     if computer_choice == "rock":
          print("You chose rock, the computer chose rock. It's a tie!")
-    elif computer_choice == "paper":
+    if computer_choice == "paper":
         print("You chose rock, the computer chose paper. You lose.")
-    elif computer_choice == "scissors":
+    if computer_choice == "scissors":
         print("You chose rock, the computer chose scissors. You win!")
 
 
-elif user_choice == "paper":
+if user_choice == "paper":
     if computer_choice == "rock":
          print("You chose paper, the computer chose rock. You win!")
-    elif computer_choice == "paper":
+    if computer_choice == "paper":
         print("You chose paper, the computer chose paper. It's a tie!")
-    elif computer_choice == "scissors":
+    if computer_choice == "scissors":
         print("You chose paper, the computer chose scissors. You lose.")
 
-elif user_choice == "scissors":
+if user_choice == "scissors":
     if computer_choice == "rock":
         print("You chose scissors, the computer chose rock. You lose.")
-    elif computer_choice == "paper":
+    if computer_choice == "paper":
         print("You chose scissors, the computer chose paper. You win!")
-    elif computer_choice == "scissors":
+    if computer_choice == "scissors":
         print("You chose scissors, the computer chose scissors. It's a tie!")
 
 
